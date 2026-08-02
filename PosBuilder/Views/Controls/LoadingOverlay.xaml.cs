95bada31f251db009c562bfc88acfb11c1ca6553
lucilfer <144937463+risejoaquin@users.noreply.github.com>
2026-08-02 12:25:28 -0700

    chore: cleanup project structure
    Remove unused patch and fix scripts and downgrade NuGet package versions for consistency.

--- PosBuilder/Views/Controls/LoadingOverlay.xaml.cs
+++ PosBuilder/Views/Controls/LoadingOverlay.xaml.cs
@@ -1,4 +1,5 @@
-using System;\nusing System.Windows;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace PosBuilder.Views.Controls
--- patch_usings.py
+++
@@ -1,7 +0,0 @@
-with open("PosBuilder/Views/Controls/LoadingOverlay.xaml.cs", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = "using System;\\n" + content
-
-with open("PosBuilder/Views/Controls/LoadingOverlay.xaml.cs", "w", encoding="utf-8") as f:
-    f.write(content)
